defa1f57aa921c69a6b7e77013910225ba2dbe16
Ruben Berenguel Montoro <ruben@mostlymaths.net>
2025-01-05 21:15:04 +0100

    Minor fixes

--- src/pico/badger/badger.cc
+++ src/pico/badger/badger.cc
@@ -423,6 +423,8 @@ void Output::CommandLine(const std::list<char>& command) {
 }
 
 void Output::CommandLine(const std::string& command) {
+  // TODO(me) This is very slow, why? It only updates a rectangle
+  // Maybe it is triggering emit from the editor?
   badger.update_speed(3);
   badger.thickness(2);
   badger.pen(15);
@@ -440,7 +442,13 @@ void Output::Emit(const std::string& current_line_str, const int cursor_pos,
   // - Compare the current line splits with the displayed line splits
   // - Anything equal at the beginning of the line stays
   // - Anything different up to the end of the line triggers a clean
-  if(current_line_str.empty() && mode == EditorMode::kInsert){
+  printf("Emit with mode: %d\n", mode);
+  if(mode == EditorMode::kCommandLineMode){
+    // Avoid refreshing the whole screen with no reason to do it
+    return;
+  }
+  if(current_line_str.empty() && mode == EditorMode::kInsert && !on_screen.empty()){
+    // TODO(me) This might still not be enough (i.e. comparing with empty screen is needed, but might not be enough)
     badger.pen(15);
     badger.clear();
     badger.update(true);
@@ -450,7 +458,6 @@ void Output::Emit(const std::string& current_line_str, const int cursor_pos,
     emits_with_no_clear = 0;
   }
   ++emits_with_no_clear;
-  //uint8_t updateFlags = 0;
   badger.pen(0);
   badger.update_speed(3);
   badger.thickness(2);
@@ -459,12 +466,10 @@ void Output::Emit(const std::string& current_line_str, const int cursor_pos,
   if (now - last_key_in < 150) {
     printf("Avoiding a fast write\n");
     last_key_in = now;
-    return;
+    return;  // TODO(me) this should not return, but call text first to make sure the screen is updated with what it _should_ have
   }
   last_key_in = now;
-  //if(fabs(on_screen.size() - current_line_str.size()) > 10){
-  //  full_update = true;
-  //}
+
   text(&badger, current_line_str, cursor_pos, 0, 0, 296, 2, mode,
        &prev_line_start_, &next_line_start_);
 
@@ -485,105 +490,6 @@ void Output::Emit(const std::string& current_line_str, const int cursor_pos,
   } else {
     printf("Badger busy\n");
   }
-
-  /*
-  std::vector<std::string> chunks;
-  int chunk_size = 22;  // eyeballing the font size
-  if (current_line_str.size() == 0) {
-    badger.pen(15);
-    badger.clear();
-    badger.update();
-    displayed_lines_.clear();
-  }
-  auto starting_index =
-      cursor_pos /
-      (chunk_size *
-       N_LINES);  // This assumes the cursor is always in the string, beware
-  printf("pos: %d / (%d) - %s\n", cursor_pos, starting_index,
-         current_line_str.c_str());
-  auto count = 0;
-  for (size_t i = starting_index * (chunk_size * N_LINES);
-       i < current_line_str.size(); i += chunk_size) {
-    chunks.push_back(current_line_str.substr(i, chunk_size));
-    ++count;  // number of chunks
-    if (count == N_LINES) {
-      break;
-    }
-  }
-
-  if (displayed_lines_.empty()) {
-    // Populate the list
-    for (int i = 0; i < fmin(N_LINES, chunks.size()); i++) {
-      // Make sure this is always populated!
-      printf("Cleaned displayed lines for #chunks %d\n", chunks.size());
-      displayed_lines_.emplace_back("");
-    }
-  }
-
-  auto diffed = StringDiffer(chunks, displayed_lines_, cursor_pos);
-  printf(("\ndiffed size: " + std::to_string(diffed.size()) + "\n").c_str());
-  for (int i = 0; i < diffed.size(); i++) {
-    if (i >= chunks.size()) {
-      if (displayed_lines_.size() >= i) {
-        badger.pen(15);
-        badger.clear();
-        badger.update(true);
-        displayed_lines_.clear();
-        printf("Full clear\n");
-        break;
-      }
-    }
-    auto diff_start = std::next(displayed_lines_[i].begin(), diffed[i]);
-    auto diff2_start = std::next(chunks[i].begin(), diffed[i]);
-    std::string same(displayed_lines_[i].begin(), diff_start);
-    if (diff_start != displayed_lines_[i].begin()) {
-      --diff_start;
-    }
-    std::string dl(displayed_lines_[i].begin(), displayed_lines_[i].end());
-    std::string diff(diff_start, displayed_lines_[i].end());
-    std::string diff2(diff2_start, chunks[i].end());
-    printf(("\ndstart: %s (%d/%d) same: %s diff: %s diff2: %s disp: %s\n"),
-           std::to_string(diffed[i]).c_str(), chunks.size(),
-           displayed_lines_.size(), same.c_str(), diff.c_str(), diff2.c_str(),
-           dl.c_str());
-    auto skip_size = badger.measure_text(same, 0.8);
-    auto clear_size = badger.measure_text(diff, 0.8);
-    auto clear_size2 = badger.measure_text(diff2, 0.8);
-    printf(("\n1: skip: " + std::to_string(skip_size) +
-            " clear: " + std::to_string(clear_size) + "\n")
-               .c_str());
-    clear_size = min(max(clear_size, clear_size2) + 24, 296);
-    skip_size = max(skip_size - 24, 0);
-    printf(("\n2: skip: " + std::to_string(skip_size) +
-            " clear: " + std::to_string(clear_size) + "\n")
-               .c_str());
-    badger.pen(15);
-    badger.rectangle(skip_size, 24 * i, clear_size, 24);
-    printf("FFS: %d %d\n", i, displayed_lines_.size());
-    if (i >= displayed_lines_.size()) {
-      displayed_lines_.push_back("");
-    }
-    displayed_lines_[i].assign(chunks[i].begin(), chunks[i].end());
-    std::string as_string(displayed_lines_[i].begin(),
-                          displayed_lines_[i].end());
-    badger.pen(0);
-    badger.text(as_string, 0, 8 + i * 24, 0.80f);
-    printf(("\n loop level: " + std::to_string(i) + " y: " +
-            std::to_string(i * 24) + ":" + std::to_string(24 + i * 24) + "\n")
-               .c_str());
-    if (diffed[i] >= 22) {  // Size of screen
-      printf("Skipping partial update for this line");
-      continue;
-    } else {
-      if (!badger.is_busy()) {
-        badger.partial_update(skip_size, i * 24, clear_size, 24 + i * 24, true);
-      } else {
-        printf("Badger busy\n");
-      }
-    }
-  }
-  */
-
 }
 
 void flush() {
